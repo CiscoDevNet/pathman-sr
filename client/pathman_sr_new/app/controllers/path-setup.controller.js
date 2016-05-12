07d14ae817dc840a9a8520c8981f46346d444f46
Alexei Zverev <alzverev@cisco.com>
2016-05-12 12:37:34 -0700

    input validation for path setup form

--- client/pathman_sr_new/app/controllers/path-setup.controller.js
+++ client/pathman_sr_new/app/controllers/path-setup.controller.js
@@ -1,11 +1,47 @@
 (function(app){
 
-	var PathSetupCtrl = function($scope) {
+	var PathSetupCtrl = function($scope, $log) {
 
-		$scope.automaticMode = true;
+		$scope.validCostMetrics = ['igp', 'hops'];
+		$scope.isAutoPathFormInvalid = isAutoPathFormInvalid;
+
+		/* Implementation */
+		function isAutoPathFormInvalid(){
+
+			if(!$scope.hasOwnProperty("psForm")){
+				return true;
+			}
+			else if(!$scope.hasOwnProperty("autoPathForm")){
+				return true;
+			}
+			else{
+
+				if($scope.autoPathForm.$invalid){
+					return true;
+				}
+
+				if($scope.psForm.hasOwnProperty('source') && $scope.psForm.hasOwnProperty('destination')){
+					if($scope.psForm.source == $scope.psForm.destination){
+						return true;
+					}
+				}
+				else{
+					return true;
+				}
+
+
+				if($scope.validCostMetrics.indexOf($scope.psForm.costMetric) === -1){
+					return true;
+				}
+			}
+
+			return false;
+
+		}
 
 	};
 
+	PathSetupCtrl.$inject = ["$scope", "$log"];
 	app.controller('PathSetupCtrl', PathSetupCtrl);
 
 })(app);
